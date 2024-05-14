763fc559be00f8c78748fbb5aac32c6ea4218e93
antares <antares@alyr.dev>
2024-05-14 23:05:55 +0800

    fix: debug mode off

--- main.py
+++ main.py
@@ -124,4 +124,4 @@ if __name__ == "__main__":
         return ret
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=port, debug=True)
+    app.run(host="127.0.0.1", port=port, debug=False)
