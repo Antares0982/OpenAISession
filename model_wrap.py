64f52f72a0b119e27dc9a7488d949bae57a06253
antares <antares@alyr.dev>
2025-02-04 16:12:07 +0800

    Update models

--- model_wrap.py
+++ model_wrap.py
@@ -5,7 +5,10 @@ GPT4_32K = 3
 GPT4_TURBO = 4
 GPT4O = 5
 GPT4O_MINI = 6
-MAX = 7
+O1 = 7
+O1_MINI = 8
+O3_MINI = 9
+MAX = 10
 
 MODEL_DICT = {
     GPT3_5: "gpt-3.5-turbo",
@@ -15,16 +18,22 @@ MODEL_DICT = {
     GPT4_TURBO: "gpt-4-turbo",
     GPT4O: "gpt-4o",
     GPT4O_MINI: "gpt-4o-mini",
+    O1: "o1",
+    O1_MINI: "o1-mini",
+    O3_MINI: "o3-mini",
 }
 
 TIKTOKEN_NAME_DICT = {
-    GPT4O: "gpt-4o",
     GPT3_5: "gpt-3.5-turbo",
     GPT3_5_16K: "gpt-3.5-turbo",
     GPT4: "gpt-4",
     GPT4_32K: "gpt-4",
     GPT4_TURBO: "gpt-4",
-    GPT4O_MINI: "gpt-4",
+    GPT4O: "gpt-4o",
+    GPT4O_MINI: "gpt-4o",
+    O1: "gpt-4o",
+    O1_MINI: "gpt-4o",
+    O3_MINI: "gpt-4o",
 }
 
 TOKEN_LIMIT_DICT = {
@@ -35,6 +44,9 @@ TOKEN_LIMIT_DICT = {
     GPT4_TURBO: 128000,
     GPT4O: 128000,
     GPT4O_MINI: 128000,
+    O1: 200000,
+    O1_MINI: 128000,
+    O3_MINI: 200000,
 }
 
 STR_MODEL_DICT = {
@@ -45,6 +57,9 @@ STR_MODEL_DICT = {
     "GPT4_TURBO": GPT4_TURBO,
     "GPT4O": GPT4O,
     "GPT4O_MINI": GPT4O_MINI,
+    "O1": O1,
+    "O1_MINI": O1_MINI,
+    "O3_MINI": O3_MINI,
 }
 
 # per 1k
@@ -54,8 +69,11 @@ PRICING_DICT = {
     GPT4: (0.03, 0.06),
     GPT4_32K: (0.06, 0.12),
     GPT4_TURBO: (0.01, 0.03),
-    GPT4O: (0.005, 0.015),
-    GPT4O_MINI: (0.00015, 0.000075),
+    GPT4O: (0.0025, 0.01),
+    GPT4O_MINI: (0.00015, 0.0006),
+    O1: (0.015, 0.06),
+    O1_MINI: (0.0011, 0.0044),
+    O3_MINI: (0.0011, 0.0044),
 }
 
 
